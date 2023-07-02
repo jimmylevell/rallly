7db092bef08b5436a8f5defc7c6552a31df8d53e
James <jimmy.levell@bluewin.ch>
2023-07-02 10:56:44 +0200

    WIP

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+# call docker secret expansion in env variables
+source /docker/set_env_secrets.sh
+
 set -e
 prisma migrate deploy --schema=./prisma/schema.prisma
 node apps/web/server.js
